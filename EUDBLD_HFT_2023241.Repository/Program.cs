eb071dfcbdf251abfdc49655d23c9bff17c60529
hallgato <hallgato@LABOR115-13>
2023-10-16 15:27:48 +0200

    Add project files.

---
+++ EUDBLD_HFT_2023241.Repository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EUDBLD_HFT_2023241.Repository
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
