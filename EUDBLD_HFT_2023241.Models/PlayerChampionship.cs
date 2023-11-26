07946eed79d0c6ca9b515ca90d1dedd39f98a7df
Bogdán Krisztián <bogdan.krisztian17@gmail.com>
2023-11-26 20:58:49 +0100

    made PlayerChampionship pivot table into models

---
+++ EUDBLD_HFT_2023241.Models/PlayerChampionship.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EUDBLD_HFT_2023241.Models
+{
+    class PlayerChampionship
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [NotMapped]
+        public virtual Player Player { get; set; }
+
+        [ForeignKey(nameof(Player))]
+        public int PlayerId { get; set; }
+
+        [NotMapped]
+        public virtual Championship Championship{ get; set; }
+
+        [ForeignKey(nameof(Championship))]
+        public int ChampionshipId { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public int Place { get; set; }
+    }
+}
