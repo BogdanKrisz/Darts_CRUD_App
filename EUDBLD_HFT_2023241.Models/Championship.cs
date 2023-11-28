6666ab6e7f96a3f08c3499d7d1215826102777a8
Bogdán Krisztián <bogdan.krisztian17@gmail.com>
2023-11-28 02:53:24 +0100

    added championships table

--- EUDBLD_HFT_2023241.Models/Championship.cs
+++ EUDBLD_HFT_2023241.Models/Championship.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EUDBLD_HFT_2023241.Models
 {
+    [Table("championships")]
     public class Championship
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("championship_id", TypeName = "int")]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(100)]
+        [Required]
+        public DateTime Date { get; set; }
     }
 }
